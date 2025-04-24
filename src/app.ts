3efd8bda8fe060ed4bebaeb1e46032046d4b4cde
ZainAli <chzainali6363@gmail.com>
2025-04-24 17:24:54 +0500

    refactor: update gRPC service handlers for AddressBook

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import path from 'path';
 import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
-import {ServiceClientConstructor} from "@grpc/grpc-js";
+import {Status} from "@grpc/grpc-js/build/src/constants";
 
 import {ProtoGrpcType} from "./generated/a";
+import {AddressBookServiceHandlers} from "./generated/AddressBookService";
 
 const packageDefinition = protoLoader.loadSync(path.join(__dirname, './a.proto'));
 
@@ -20,28 +21,32 @@ const PERSONS = [
     },
 ];
 
-// @ts-ignore
-function addPerson(call, callback) {
-    let person = {
-        name: call.request.name,
-        age: call.request.age
-    }
-    PERSONS.push(person);
-    callback(null, person)
-}
+const handler: AddressBookServiceHandlers = {
+    AddPerson: (call, callback) => {
+        let person = {
+            name: call.request.name,
+            age: call.request.age
+        }
+        PERSONS.push(person);
+        callback(null, person)
+    },
 
-// @ts-ignore
-function getPersonByName(call, callback) {
-    const person = PERSONS.find(p => p.name === call.request.name);
-    callback(null, person ? person : {message: "Person not found"});
-}
+    GetPersonByName: (call, callback) => {
+        let person = PERSONS.find(x => x.name === call.request.name);
+        if (person) {
+            callback(null, person)
+        } else {
+            callback({
+                code: Status.NOT_FOUND,
+                details: "not found"
+            }, null);
+        }
+    },
+};
 
 const server = new grpc.Server();
 
-server.addService((personProto.AddressBookService as ServiceClientConstructor).service, {
-    addPerson: addPerson,
-    getPersonByName: getPersonByName
-});
+server.addService((personProto.AddressBookService).service, handler);
 
 server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
     console.log('Server running at http://0.0.0.0:50051');
